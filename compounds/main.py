ee2ae3c0d68af101dcb6b121662a4bea056aa31b
sebastian <sebastian.burgstaller@gmail.com>
2015-07-29 22:57:39 -0700

    main.py added as caller for drug data aggreagator and drug bot

---
+++ compounds/main.py
@@ -0,0 +1,10 @@
+__author__ = 'sebastian'
+
+import sys
+from compounds.drug_data_aggregator import DrugDataAggregator
+
+def main():
+    DrugDataAggregator()
+
+if __name__ == '__main__':
+    sys.exit(main())
