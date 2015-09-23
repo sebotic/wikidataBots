b7561747c8af950b67c085856520455ffc1a082c
Tim Putman <putmantime@gmail.com>
2015-09-23 09:20:59 -0700

    w

---
+++ genes/microbes/gene_microbot2.py
@@ -0,0 +1,12 @@
+#!/usr/local/bin/python3
+
+import PBB_Core
+import PBB_login
+
+__author__ = 'timputman'
+
+login = PBB_login.WDLogin('Putmantime', 'TP975310tp')
+
+item_name = 'Vemurafenib'
+
+value = PBB_Core.WDString
