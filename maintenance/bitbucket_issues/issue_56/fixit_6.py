032c4dc2584dc24e36425850d6ff8d788a77e137
Andra Waagmeester <andra@micelio.be>
2015-12-20 19:23:09 +0100

    various fix bots : Issue: 54 Issue: 56

--- maintenance/bitbucket_issues/issue_56/fixit_6.py
+++ maintenance/bitbucket_issues/issue_56/fixit_6.py
@@ -1,5 +1,7 @@
 __author__ = 'andra'
 
+# This is a maintenance bot which removes all occurences where a protein is incorrectly being encoded by another protein
+
 import time
 import sys
 import os
@@ -7,36 +9,41 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__))+"/../../../ProteinBox
 import PBB_login
 import PBB_settings
 import PBB_Core
+import pprint
+import traceback
 
 from SPARQLWrapper import SPARQLWrapper, JSON
 
 logincreds = PBB_login.WDLogin(PBB_settings.getWikiDataUser(), PBB_settings.getWikiDataPassword())
-counter = 0
+
 sparql = SPARQLWrapper("https://query.wikidata.org/bigdata/namespace/wdq/sparql")
 sparql.setQuery("""
-     PREFIX wikibase: <http://wikiba.se/ontology#>
+PREFIX wikibase: <http://wikiba.se/ontology#>
 PREFIX wd: <http://www.wikidata.org/entity/>
 PREFIX wdt: <http://www.wikidata.org/prop/direct/>
 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 PREFIX p: <http://www.wikidata.org/prop/>
 PREFIX v: <http://www.wikidata.org/prop/statement/>
 
-SELECT distinct ?gene ?protein WHERE {
-    ?gene wdt:P703 wd:Q83310 ;
-          wdt:P688 ?protein .
-    ?protein wdt:P703 wd:Q5 .
+SELECT DISTINCT ?protein {
+?protein wdt:P702 ?otherprotein .
+?otherprotein wdt:P279 wd:Q8054 .
 }
 """)
+
 sparql.setReturnFormat(JSON)
 results = sparql.query().convert()
+
+# pprint.pprint(results)
+counter = 0
 for result in results["results"]["bindings"]:
   try:
         counter = counter + 1
-        print(result["gene"]["value"])
-        gene = result["gene"]["value"].replace("http://www.wikidata.org/entity/", "")
-        data2add = [PBB_Core.WDBaseDataType.delete_statement(prop_nr='P688')]
-        wdPage = PBB_Core.WDItemEngine(gene, data=data2add, server="www.wikidata.org",
-                                           domain="genes")
+        print(result["protein"]["value"])
+        protein = result["protein"]["value"].replace("http://www.wikidata.org/entity/", "")
+        data2add = [PBB_Core.WDBaseDataType.delete_statement(prop_nr='P702')]
+        wdPage = PBB_Core.WDItemEngine(protein, data=data2add, server="www.wikidata.org",
+                                           domain="proteins")
         wdPage.write(logincreds)
 
   except Exception as e:
--- maintenance/bitbucket_issues/issue_56/fixit_7.py
+++ maintenance/bitbucket_issues/issue_56/fixit_7.py
@@ -1,5 +1,7 @@
 __author__ = 'andra'
 
+# This is a maintenance bot which removes all occurences where a protein is incorrectly being encoded by another protein
+
 import time
 import sys
 import os
@@ -7,28 +9,49 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__))+"/../../../ProteinBox
 import PBB_login
 import PBB_settings
 import PBB_Core
+import pprint
+import traceback
 
 from SPARQLWrapper import SPARQLWrapper, JSON
 
 logincreds = PBB_login.WDLogin(PBB_settings.getWikiDataUser(), PBB_settings.getWikiDataPassword())
-counter = 0
+
 sparql = SPARQLWrapper("https://query.wikidata.org/bigdata/namespace/wdq/sparql")
 sparql.setQuery("""
-     PREFIX wikibase: <http://wikiba.se/ontology#>
+PREFIX wikibase: <http://wikiba.se/ontology#>
 PREFIX wd: <http://www.wikidata.org/entity/>
 PREFIX wdt: <http://www.wikidata.org/prop/direct/>
 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 PREFIX p: <http://www.wikidata.org/prop/>
 PREFIX v: <http://www.wikidata.org/prop/statement/>
 
-SELECT distinct ?gene ?protein WHERE {
-    ?gene wdt:P703 wd:Q83310 ;
+SELECT ?gene ?uniprot ?protein_aff ?total WHERE {
+  {
+  SELECT distinct ?uniprot (count(?uniprot) as ?total) WHERE {
+    ?gene wdt:P703 wd:Q5 ;
+          wdt:P279 wd:Q7187 ;
           wdt:P688 ?protein .
-    ?protein wdt:P703 wd:Q5 .
-}
+    ?protein wdt:P352 ?uniprot ;
+             wdt:P279 wd:Q8054 ;
+             wdt:P703 wd:Q5 .
+    }
+    Group BY ?uniprot
+  }
+
+  ?protein_aff wdt:P352 ?uniprot .
+  ?gene wdt:P688 ?protein_aff ;
+        wdt:P703 wd:Q5 .
+
+  FILTER (?total > 1)
+ }
+ ORDER BY ?gene
 """)
+
 sparql.setReturnFormat(JSON)
 results = sparql.query().convert()
+
+# pprint.pprint(results)
+counter = 0
 for result in results["results"]["bindings"]:
   try:
         counter = counter + 1
