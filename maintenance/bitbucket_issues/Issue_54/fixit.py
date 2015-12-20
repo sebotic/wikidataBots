5bf998a2b8b861587049403382d8f91987f2d81c
Andra Waagmeester <andra@micelio.be>
2015-12-20 21:38:15 +0100

    Issue 54

--- maintenance/bitbucket_issues/Issue_54/fixit.py
+++ maintenance/bitbucket_issues/Issue_54/fixit.py
@@ -7,6 +7,7 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__))+"/../../../ProteinBox
 import PBB_login
 import PBB_settings
 import PBB_Core
+import traceback
 
 from SPARQLWrapper import SPARQLWrapper, JSON
 
